bda3e839482dee6f3de7aefa256e5354dbb8a1e7
seawavve <hanhee.sw@gmail.com>
2022-03-11 16:49:59 +0900

    multiprocessed

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import torch
 import math
 import time
 from pydantic import BaseModel
+from multiprocessing import Pool
 
 app = FastAPI()
 db = dict()
@@ -31,13 +32,33 @@ class Item(BaseModel):
 # #     return {'sentences':db,'predict_result':predict_result}
 
 
+# @app.post("/items/")
+# async def get_text(item:Item):
+#     start = time.time()
+#     db = item.dict()
+#     length = len(db['sentence_1']) + len(db['sentence_2'])
+#     end = time.time()
+#     runtime = end - start
+#     return {'length_result':length, 'runtime(sec)':runtime}
+
+def get_length(sent1,sent2):
+    return len(sent1) + len(sent2)
+
 @app.post("/items/")
-def get_text(item:Item):
+async def get_json(item:Item):
     start = time.time()
+    p=Pool()
     db = item.dict()
+    
     length = len(db['sentence_1']) + len(db['sentence_2'])
+    ret1 = 1
+    ret2 = 2
+    ret1 = p.apply(get_length,(db['sentence_1'],db['sentence_2'],))
+    ret2 = p.apply(get_length,(db['sentence_1'],db['sentence_1'],))
+    
+    
     end = time.time()
     runtime = end - start
-    return {'length_result':length, 'runtime(sec)':runtime}
-
-# model inference
+    p.close()
+    p.join()
+    return {'length_result':length, 'runtime(sec)':runtime,'ret1': ret1, 'ret2':ret2}
