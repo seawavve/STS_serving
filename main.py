f7ebcbad6f7a2a178466093622a12f5b5d87374b
seawavve <hanhee.sw@gmail.com>
2022-03-11 16:12:22 +0900

    ready for multiprocessing

--- main.py
+++ main.py
@@ -1,33 +1,43 @@
 from fastapi import FastAPI
 from fastapi import UploadFile
 from fastapi import File
-
+import torch
+import math
+import time
 from pydantic import BaseModel
-import requests
 
 app = FastAPI()
-db=dict()
+db = dict()
 
 class Item(BaseModel):
     sentence_1: str
     sentence_2: str
 
-@app.get("/")
-async def show_2sent():
-    sents=db
-    return sents
+# @app.get("/")
+# async def show_2sent():
+    
+#     sents = db
+#     return sents
+
+# @app.get("/predict")
+# async def predict(db):
+#     PATH = 'model_tutorial.pt'
+#     model = torch.load(PATH)
+#     model.eval()
+#     logits = model(db.values())
+#     return logits
+# #     model = 'model_tutorial.pt'
+# #     predict_result = inference.inference(model,db.values())
+# #     return {'sentences':db,'predict_result':predict_result}
+
 
 @app.post("/items/")
 def get_text(item:Item):
-    item_dict = item.dict()
-    db = item_dict
-    print(db)
-    # get 2 line text
-    # image = np.array(Image.open(file.file))
-    # model = config.STYLES[style]
-    # output, resized = inference.inference(model, image)
-    # name = f"/storage/{str(uuid.uuid4())}.jpg"
-    # cv2.imwrite(name, output)
-    return item
+    start = time.time()
+    db = item.dict()
+    length = len(db['sentence_1']) + len(db['sentence_2'])
+    end = time.time()
+    runtime = end - start
+    return {'length_result':length, 'runtime(sec)':runtime}
 
 # model inference
---
+++ pool_test.py
@@ -0,0 +1,5 @@
+import multiprocessing
+
+cpu_count = multiprocessing.cpu_count()  # 2
+print(f'CPU : {cpu_count} ')
+print(cpu_count)
