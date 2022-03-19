6d6b6a65f501fadb1fe0df2c915c1c71b38ee07b
seawavve <hanhee.sw@gmail.com>
2022-03-19 17:18:10 +0900

    create predict

---
+++ test.py
@@ -0,0 +1,36 @@
+# # uvicorn main:app --reload
+
+# from fastapi import FastAPI
+# from fastapi import UploadFile
+# from fastapi import File
+
+# from pydantic import BaseModel
+# from multiprocessing import Pool
+# from fastapi import FastAPI, File, UploadFile
+
+# # from content import get_length, predict_model
+# from fastapi.middleware.cors import CORSMiddleware
+from sentence_transformers import SentenceTransformer
+
+
+# print('test23432')
+
+# app = FastAPI()
+# db = dict()
+
+# origins = [
+#     "https://localhost",
+#     "http://localhost",
+#     "http://localhost:8000",
+#     "https://localhost:8000",
+#     'http://127.0.0.1:8000'
+# ]
+
+
+# @app.post("/items/")
+# async def get_text():
+    
+#     return {}
+
+# from sentence_transformers import SentenceTransformer
+print('test223')
